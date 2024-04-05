f43e432ac7f235809c1acd1f5975612146e0d2d7
Heewon-K <heewon.ms.kim@gmail.com>
2024-04-05 19:07:34 +0900

    add redirect

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='waterlog/show_logs/')),
     path("admin/", admin.site.urls),
     path('waterlog/', include('waterlog.urls')),
 ]
