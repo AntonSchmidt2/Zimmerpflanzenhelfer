76cfd249023df17ee3a0a7a3693db260ea7b8cc6
Anton Schmidt <schmidt.anton2@gmail.com>
2024-04-17 20:52:56 +0200

    adapted views and html to the new models

--- waterlog/models.py
+++ waterlog/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from datetime import datetime
 
 class Plant(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default="SomePlant")
     species = models.CharField(max_length=100, blank=True, null=True)
-    location = models.CharField(max_length=100)
-    last_watered = models.DateField(auto_now_add=True)
+    location = models.CharField(max_length=100, null=True)
+    last_watered = models.DateField(auto_now_add=True, null=True)
     watering_interval = models.IntegerField(help_text="Enter number of days between watering")
 
     def __str__(self):
         return self.name
 
 class WaterLog(models.Model):
-    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
-    watered_at = models.DateTimeField()
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE, null=True)  # Making 'plant' nullable
+    watered_at = models.DateTimeField(default=datetime.now)  # Default value for 'watered_at'
 
     def __str__(self):
         return f"{self.plant.name} watered on {self.watered_at.strftime('%Y-%m-%d %H:%M')}"
