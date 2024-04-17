65db7643c0c392fff21c1f3c2f3bbb9d073363ad
Anton Schmidt <schmidt.anton2@gmail.com>
2024-04-17 20:28:22 +0200

    added a more sophisticated model

--- waterlog/models.py
+++ waterlog/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=100)
+    last_watered = models.DateField(auto_now_add=True)
+    watering_interval = models.IntegerField(help_text="Enter number of days between watering")
+
+    def __str__(self):
+        return self.name
+
 class WaterLog(models.Model):
-    plant_name = models.CharField(max_length=100)
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
     watered_at = models.DateTimeField()
 
     def __str__(self):
-        return self.plant_name
+        return f"{self.plant.name} watered on {self.watered_at.strftime('%Y-%m-%d %H:%M')}"
